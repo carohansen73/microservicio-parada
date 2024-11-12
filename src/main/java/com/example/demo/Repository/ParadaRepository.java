08a565e7db5d5cf23563071d30fe825ee720e0ee
Facundo <facundovis@gmail.com>
2024-11-12 03:01:23 -0300

    Agrego funcion gps en el service de parada

--- src/main/java/com/example/demo/Repository/ParadaRepository.java
+++ src/main/java/com/example/demo/Repository/ParadaRepository.java
@@ -1,7 +1,11 @@
 package com.example.demo.Repository;
 
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.example.demo.modelo.Parada;
@@ -9,5 +13,10 @@ import com.example.demo.modelo.Parada;
 //No uso JPa porque es NoSQL
 @Repository
 public interface ParadaRepository extends MongoRepository<Parada,Long> {
-
+	
+	@Query(value = "{}", sort = "{ $add: [ { $pow: [ { $subtract: [ :latitud , latitud ] }, 2 ] }, { $pow: [ { $subtract: [ :longitud , longitud ] }, 2 ] } ] }")
+    List<Parada> findParadaMasCercana(@Param("latitud") long latitud, @Param("longitud") long longitud);
+	
+	@Query("{ 'latitud': ?0, 'longitud': ?1 }")
+    Parada findByLatitudAndLongitud(long latitud, long longitud);
 }
--- src/main/java/com/example/demo/service/ParadaService.java
+++ src/main/java/com/example/demo/service/ParadaService.java
@@ -1,6 +1,8 @@
 package com.example.demo.service;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -39,6 +41,20 @@ public class ParadaService {
 			  throw new IllegalArgumentException("Parada no encontrada.");
 		}
 	}
+	public List<Parada> findParadaMasCercana(long latitud, long longitud) {
+        return (List<Parada>) repository.findAll().stream().sorted((p1, p2) -> Double.compare(
+                calcularDistancia(latitud, longitud, p1.getLatitud(), p1.getLongitud()),
+                calcularDistancia(latitud, longitud, p2.getLatitud(), p2.getLongitud())
+            ))
+            .collect(Collectors.toList());
+    }
+
+    private double calcularDistancia(long lat1, long lon1, double d, double e) {
+        return Math.sqrt(Math.pow(d - lat1, 2) + Math.pow(e - lon1, 2));
+    }
 	
+    public Boolean estaEnLaParada(long latitud, long longitud) {
+        return repository.findByLatitudAndLongitud(latitud, longitud) != null;
+    }
 	
 }
